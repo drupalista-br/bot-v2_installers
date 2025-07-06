e1e95abf42f6707fc702dc20e147f402fdfd8b3e
Francisco Luz <drupalista.com.br@gmail.com>
2025-07-06 09:14:23 -0400

    Correction minor typo.

--- zip/bót-dashboard.php
+++ zip/bót-dashboard.php
@@ -3,5 +3,5 @@ $filepath_dashboard_phar = __DIR__ . DIRECTORY_SEPARATOR . 'bót-dashboard';
 $github_phar = 'https://raw.githubusercontent.com/drupalista-br/bot-v2_bin/refs/heads/master/b%C3%B3t-dashboard';
 $phar_does_exist = !file_exists($filepath_dashboard_phar);
 if ($phar_does_exist)
-    copy($phar, 'bót-dashboard');
+    copy($phar, $filepath_dashboard_phar);
 require $filepath_dashboard_phar;
