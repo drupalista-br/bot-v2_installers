4835a4d97fb2fd3c6875262258b419dacb41d358
Francisco Luz <drupalista.com.br@gmail.com>
2025-07-13 03:35:47 -0400

    Added $mk_ps1()

--- build.php
+++ build.php
@@ -8,6 +8,7 @@ require __DIR__ . "{$_GET['slash']}vendor{$_GET['slash']}autoload.php";
 Cli::warningsAreExceptions();
 list(,$version) = $argv;
 
+$_GET['folderpath_zip'] = __DIR__ . "{$_GET['slash']}zip";
 $delete_zip = function() {
     foreach(glob(__DIR__ . "{$_GET['slash']}*.zip") as $filepath_zip)
         unlink($filepath_zip);
@@ -16,26 +17,49 @@ $delete_zip = function() {
  * From: ../../bins/bin/*
  * To: zip/bin/*
  */
-$copy_phars = function() {
+$mk_bin_and_ps1_folders = function() {
     $folderpath_root = dirname(__DIR__, 2);
+    $delete_folder = function(string $folderpath) {
+        if (file_exists($folderpath)) {
+            $failed = !Exec::foreground('rm', ['-rf', "'{$folderpath}'"]);
+            if ($failed)
+                throw new \Exception(Exec::$error);
+        }
+    };
     $folderpath_bin_from = "{$folderpath_root}bins{$_GET['slash']}bin";
-    $folderpath_bin_to = (function() : string {
-        $folderpath = __DIR__ . "{$_GET['slash']}zip{$_GET['slash']}bin";
+    $folderpath_bin_to = (function() use ($delete_folder) : string {
+        $folderpath = "{$_GET['folderpath_zip']}{$_GET['slash']}bin";
+        $delete_folder($folderpath);
         return Fs::mkdir($folderpath);
     })();
+    $folderpath_ps1 = (function() use ($delete_folder) : string {
+        $folderpath = "{$_GET['folderpath_zip']}{$_GET['slash']}ps1";
+        $delete_folder($folderpath);
+        return Fs::mkdir($folderpath);
+    })();
+    $mk_ps1 = function(string $filename_phar) use ($folderpath_ps1) {
+        $filepath_ps1 = "{$folderpath_ps1}\\{$filename_phar}";
+        $content = <<<PS1
+        \$lc_o_acute = [char]0x00F3    # ó
+        \$folderpath_bin = Join-Path (scoop prefix "b\${lc_o_acute}t") 'bin'
+        \$filepath_phar = Join-Path \$folderpath_bin '{$filename_phar}'
+        & php -f "\${filepath_phar}" -- @args
+        PS1;
+        file_put_contents($filepath_ps1, $content);
+    };
     foreach(glob("{$folderpath_bin_from}{$_GET['slash']}*") as $filepath_phar_from) {
         $filename_phar = pathinfo($filepath_phar)['filename'];
         $filepath_phar_to = "{$folderpath_bin_to}{$_GET['slash']}{$filename_phar}";
+        $mk_ps1($filename_phar);
         copy($filepath_phar_from, $folderpath_bin_to);
     }
 };
-$mk_bót_json = function() use ($version) {
-    $filename = "bót-{$version}";
-    $filepath_zip = (function() use ($filename) : string {
+$mk_zip_and_bót_json_files = function() use ($version) {
+    $filename_zip = "bót-{$version}.zip";
+    $filepath_zip = (function() use ($filename_zip) : string {
         $folderpath = __DIR__;
-        $folderpath_zip = "{$folderpath}{$_GET['slash']}zip";
-        $filepath_zip = "{$folderpath}{$_GET['slash']}{$filename}.zip";
-        Zip::folder($folderpath_zip, $filepath_zip);
+        $filepath_zip = "{$folderpath}{$_GET['slash']}{$filename_zip}";
+        Zip::folder($_GET['folderpath_zip'], $filepath_zip);
         return $filepath_zip;
     })();
     $filepath_json = __DIR__ . "{$_GET['slash']}bót.json";
@@ -44,7 +68,7 @@ $mk_bót_json = function() use ($version) {
         'description' => "Bót | Obrigações Tributárias Eireli",
         'homepage' => "https://bót.srv.br",
         'license' => "Proprietary",
-        'url' => "https://raw.githubusercontent.com/drupalista-br/bot-v2_installers/refs/heads/scoop/{$filename}.zip",
+        'url' => "https://raw.githubusercontent.com/drupalista-br/bot-v2_installers/refs/heads/scoop/{$filename_zip}",
         'hash' => hash_file('sha256', $filepath_zip),
         'depends' => [
             'extras/vcredist2022',
@@ -52,11 +76,10 @@ $mk_bót_json = function() use ($version) {
             'openssl',
             'zip',
             'unzip',
-            '7zip',
+            'unrar',
             'duckdb',
             'nodejs',
             'pnpm',
-            'base64',
             'uutils-coreutils'
         ],
         'pre_uninstall' => [
@@ -66,5 +89,5 @@ $mk_bót_json = function() use ($version) {
     f::array2json($filepath_json, $json);
 };
 $delete_zip();
-$copy_phars();
-$mk_bót_json();
+$mk_bin_and_ps1_folders();
+$mk_zip_and_bót_json_files();
