78baa92690df0b7cd480b7b2ee64131fe546174c
Francisco Luz <drupalista.com.br@gmail.com>
2025-07-14 12:37:11 -0400

    Converted {$_GET['slash']} into /

--- build.php
+++ build.php
@@ -1,13 +1,12 @@
 <?php
 use Bót\Utils\Cli;
 use Bót\Utils\Fs;
-$_GET['slash'] = DIRECTORY_SEPARATOR;
 require __DIR__ . "/vendor/autoload.php";
 Cli::warningsAreExceptions();
 [$version] = Cli::args($argv, total_expected: 1);
 
 $_GET['folderpath_bin'] = (function() : string {
-    $folderpath = __DIR__ . "{$_GET['slash']}bin";
+    $folderpath = __DIR__ . "/bin";
     return Fs::mkdir($folderpath);
 })();
 /**
@@ -16,18 +15,18 @@ $_GET['folderpath_bin'] = (function() : string {
  */
 $copy_phars = function() {
     $folderpath_root = dirname(__DIR__, 2);
-    $folderpath_bin_from = "{$folderpath_root}bins{$_GET['slash']}bin";
-    foreach(glob("{$folderpath_bin_from}{$_GET['slash']}*") as $filepath_phar_from) {
+    $folderpath_bin_from = "{$folderpath_root}bins/bin";
+    foreach(glob("{$folderpath_bin_from}/*") as $filepath_phar_from) {
         $filename_phar = pathinfo($filepath_phar)['filename'];
-        $filepath_phar_to = "{$_GET['folderpath_bin']}{$_GET['slash']}{$filename_phar}";
+        $filepath_phar_to = "{$_GET['folderpath_bin']}/{$filename_phar}";
         copy($filepath_phar_from, $_GET['folderpath_bin']);
     }
 };
 $mk_bot_nix = function() use ($version) {
-    $filepath = __DIR__ . "{$_GET['slash']}bót.nix";
+    $filepath = __DIR__ . "/bót.nix";
     $phars = (function() : string {
         $return = '';
-        foreach(glob("{$_GET['folderpath_bin']}{$_GET['slash']}*") as $filepath_phar) {
+        foreach(glob("{$_GET['folderpath_bin']}/*") as $filepath_phar) {
             $filename = basename($filepath_phar);
             $return .= "cp \"\${src}/bin/{$filename}\" \"\$out/bin/{$filename}\"\n";
         }
