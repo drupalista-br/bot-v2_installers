8d01102d41ff9ee7a6dd99d68e8a58c521ab312c
Francisco Luz <drupalista.com.br@gmail.com>
2025-07-28 18:52:28 -0400

    Refactoring...

--- build.php
+++ build.php
@@ -1,45 +1,31 @@
 <?php
 use Bót\Utils\Args;
 use Bót\Utils\Zip;
-use Bót\Utils\Fs;
 use Bót\Utils\f;
 require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . '/../nixos/functions.php';
 f::warningsAreExceptions();
 [$version] = Args::fixedNumber($argv, total_expected: 1);
-
 $_GET['folderpath_zip'] = __DIR__ . "/zip";
-$delete_zip = function() {
+$filename_zip = "bót-{$version}.zip";
+$filepath_zip = function() use ($filename_zip) : string {
+    $folderpath = __DIR__;
+    $filepath_zip = "{$folderpath}/{$filename_zip}";
+    Zip::folder($_GET['folderpath_zip'], $filepath_zip);
+    return $filepath_zip;
+};
+$delete_zip_file = function() {
     foreach(glob(__DIR__ . "/*.zip") as $filepath_zip)
         unlink($filepath_zip);
 };
-/**
- * From: ../../bins/bin/*
- * To: zip/bin/*
- */
-$mk_bin_and_ps1_folders = function() {
-    $folderpath_root = dirname(__DIR__, 2);
-    $delete_folder = function(string $folderpath) {
-        if (file_exists($folderpath)) {
-            $failed = !Exec::foreground('rm', ['-rf', "'{$folderpath}'"]);
-            if ($failed)
-                throw new \Exception(Exec::$error);
-        }
-    };
-    $folderpath_bin = [
-        'from' => "{$folderpath_root}bins/bin",
-        'to' => (function() use ($delete_folder) : string {
-            $folderpath = "{$_GET['folderpath_zip']}/bin";
-            $delete_folder($folderpath);
-            return Fs::mkdir($folderpath);
-        })(),
-    ];
-    $folderpath_ps1 = (function() use ($delete_folder) : string {
+$mk_exe_PS1s = function() {
+    $folderpath_ps1 = (function() : string {
         $folderpath = "{$_GET['folderpath_zip']}/ps1";
-        $delete_folder($folderpath);
-        return Fs::mkdir($folderpath);
+        mkdir($folderpath, recursive: TRUE);
+        return $folderpath;
     })();
-    $mk_bot_browser_ps1 = function() use ($folderpath_ps1) {
-        $filepath_ps1 = "{$folderpath_ps1}\\bót-browser.ps1";
+    $mk_bót_browser_ps1 = function() use ($folderpath_ps1) {
+        $filepath_ps1 = "{$folderpath_ps1}/bót-browser.ps1";
         $content = <<<PS1
         param(
             [Parameter(ValueFromRemainingArguments = \$true)]
@@ -52,35 +38,41 @@ $mk_bin_and_ps1_folders = function() {
                 break
             }
         }
+
         PS1;
         file_put_contents($filepath_ps1, $content);
     };
-    $mk_ps1 = function(string $filename_phar) use ($folderpath_ps1) {
-        $filepath_ps1 = "{$folderpath_ps1}\\{$filename_phar}";
-        $content = <<<PS1
-        \$lc_o_acute = [char]0x00F3    # ó
-        \$folderpath_bin = Join-Path (scoop prefix "b\${lc_o_acute}t") 'bin'
-        \$filepath_phar = Join-Path \$folderpath_bin '{$filename_phar}'
-        & php -f "\${filepath_phar}" -- @args
-        PS1;
-        file_put_contents($filepath_ps1, $content);
+    $mk_exe_ps1 = function(string $folderpath) use ($folderpath_ps1) {
+        $mk_ps1 = function(string $filename_phar) use ($folderpath_ps1) {
+            $filepath_ps1 = "{$folderpath_ps1}/{$filename_phar}";
+            $content = <<<PS1
+            \$lc_o_acute = [char]0x00F3    # ó
+            \$folderpath_bin = Join-Path (scoop prefix "b\${lc_o_acute}t") 'bin'
+            \$filepath_phar = Join-Path \$folderpath_bin '{$filename_phar}'
+            & php -f "\${filepath_phar}" -- @args
+
+            PS1;
+            file_put_contents($filepath_ps1, $content);
+        };
+        foreach(glob("{$folderpath}/*") as $filepath_phar_from) {
+            $filename_phar = pathinfo($filepath_phar_from)['filename'];
+            $mk_ps1($filename_phar);
+        }
     };
-    $mk_bot_browser_ps1();
-    foreach(glob("{$folderpath_bin['from']}/*") as $filepath_phar_from) {
-        $filename_phar = pathinfo($filepath_phar)['filename'];
-        $filepath_phar_to = "{$folderpath_bin['to']}/{$filename_phar}";
-        $mk_ps1($filename_phar);
-        copy($filepath_phar_from, $folderpath_bin['to']);
+    $is_js_folder = fn(string $folderpath) : bool => pathinfo($folderpath)['filename'] === 'js';
+    $mk_bót_browser_ps1();
+    foreach(glob(Functions::folderpathBins() . '/*') as $folderpath) {
+        if ($is_js_folder($folderpath))
+            continue;
+        $mk_exe_ps1($folderpath);
     }
 };
-$mk_zip_and_bót_json_files = function() use ($version) {
-    $filename_zip = "bót-{$version}.zip";
-    $filepath_zip = (function() use ($filename_zip) : string {
-        $folderpath = __DIR__;
-        $filepath_zip = "{$folderpath}/{$filename_zip}";
-        Zip::folder($_GET['folderpath_zip'], $filepath_zip);
-        return $filepath_zip;
-    })();
+$copy_from_local = function() {
+    $filenames = ['desinstalar.ps1', 'logo.ico', 'atalhos.json'];
+    foreach($filenames as $filename)
+        copy(__DIR__ . "/{$filename}", "{$_GET['folderpath_zip']}/{$filename}");
+};
+$mk_bót_json = function(string $filepath_zip) use ($version, $filename_zip) {
     $filepath_json = __DIR__ . "/bót.json";
     $json = [
         'version' => $version,
@@ -109,6 +101,9 @@ $mk_zip_and_bót_json_files = function() use ($version) {
     ];
     f::array2json($filepath_json, $json);
 };
-$delete_zip();
-$mk_bin_and_ps1_folders();
-$mk_zip_and_bót_json_files();
+Functions::delete(__DIR__, ['zip']);
+Functions::copyFromBinsTo($_GET['folderpath_zip'], ['js']);
+$delete_zip_file();
+$mk_exe_PS1s();
+$copy_from_local();
+$mk_bót_json($filepath_zip());
