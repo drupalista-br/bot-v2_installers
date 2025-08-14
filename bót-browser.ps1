2b7830b930635d8fdea232799987918855a2e9cf
Francisco Luz <drupalista.com.br@gmail.com>
2025-08-14 04:10:12 -0400

    Refactoring...

--- bót-browser.ps1
+++ bót-browser.ps1
@@ -2,24 +2,46 @@ param(
     [Parameter(ValueFromRemainingArguments = $true)]
     [string[]]$_args
 )
-$browsers = @("chrome", "chromium")
-foreach ($browser in $browsers) {
-    $filepath_exe = & where.exe $browser 2>$null
-    $launch = {
-        $is_scoop = $filepath_exe -like "*\scoop\shims\*"
-        $filepath_exe_scoop = { Join-Path (scoop prefix $browser) "${browser}.exe" }
-        $filepath_exe = &{
-            if ($is_scoop) {
-                return &$filepath_exe_scoop
-            }
-            else {
+$browser_names = @("chrome", "chromium")
+$filepath_where = {
+    param($browser_name)
+    $output = where.exe $browser_name 2>$null
+    if ($output.Count -gt 1) {
+        return $output[0]
+    }
+    return $output
+}
+$get_filepath_scoop = {
+    param($folderpath_prefix)
+    $filepath_exe = {
+        foreach ($browser_name in $browser_names) {
+            $filepath_exe = Join-Path $folderpath_prefix "${browser_name}.exe"
+            if (Test-Path -Path $filepath_exe) {
                 return $filepath_exe
             }
         }
-        Start-Process $filepath_exe -ArgumentList $_args
+        return $null
     }
+    if ($folderpath_prefix) {
+        return &$filepath_exe
+    }
+    return $null
+}
+$launched = $false
+foreach ($browser_name in $browser_names) {
+    # `scoop prefix` will stdout `Could not find app path for 'BROWSER_NAME'`
+    # instead of stderr. Ignore it.
+    $folderpath_prefix = scoop prefix $browser_name 2>$null
+    $filepath_scoop = &$get_filepath_scoop $folderpath_prefix
+    $filepath_exe = if ($filepath_scoop) {$filepath_scoop} else {&$filepath_where $browser_name}
     if ($filepath_exe) {
-        &$launch
+        $launched = $true
+        Write-Host "exe: '${filepath_exe}'"
+        Start-Process $filepath_exe -ArgumentList $_args
         break
     }
+}
+if (-not $launched) {
+    Write-Host "Instale o Chromium com o comando 'scoop install extras/chromium'"
+    exit 1
 }
